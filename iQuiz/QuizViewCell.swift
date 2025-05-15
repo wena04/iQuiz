dcebcdf91b308b867e08bc926a8919baba1599c9
wena04 <wena04@uw.edu>
2025-05-14 23:12:40 -0700

    part 2?

---
+++ iQuiz/QuizViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuizViewCell.swift
+//  iQuiz
+//
+//  Created by Anthony  Wen on 5/14/25.
+//
+
+import UIKit
+
+class QuizViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
